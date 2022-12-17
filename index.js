5035496fc3af00242341341fa053cc14666e406a
Tomas Bivainis <bivainis.tomas@gmail.com>
2022-12-17 18:16:04 +0200

    expanded book div, added a class book to books

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let btnExit = document.getElementById("exit");
 let container = document.getElementById("container");
 let form = document.getElementById("form");
 let formInputs = document.querySelectorAll("form input");
-let myLibrary = [{title: "Harry Potter", authro: "J. K. Rowlling", pages: 300, read: true}];
+let myLibrary = [{title: "Harry Potter", author: "J. K. Rowlling", pages: 300, read: true}];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -24,7 +24,27 @@ function displayBooks() {
 
   myLibrary.forEach(i => {
     let bookDiv = document.createElement("div");
-    bookDiv.innerHTML = i.title;
+
+    bookDiv.classList.add("book");
+
+    let title = document.createElement("h1");
+    title.innerHTML = i.title;
+
+    let author = document.createElement("h3");
+    author.innerHTML = i.author;
+
+    let pages = document.createElement("p");
+    pages.innerHTML = `${i.pages} pages`;
+    
+    let read = document.createElement("p");
+    if(read) read.innerHTML = "The book is read.";
+    else read.innerHTML = "The book is not read.";
+
+    bookDiv.append(title);
+    bookDiv.append(author);
+    bookDiv.append(pages);
+    bookDiv.append(read);
+
     booksDiv.append(bookDiv);
   })
 }
